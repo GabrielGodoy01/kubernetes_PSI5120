c6621f5e3867648fd7511a9877a94e3ba1420580
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-08-10 02:48:48 -0300

    deploys

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,14 @@ RUN apt-get update && apt-get install -y \
 # Definir o diretório de trabalho dentro do container como /app
 WORKDIR /app
 
-# Copiar o arquivo de requisitos para o diretório de trabalho no container
+# Copia o requirements.txt e instala as dependências
 COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
 
 # Instalar as dependências Python listadas em requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install cython pillow>=7.0.0 numpy>=1.18.1 opencv-python>=4.1.2 torch>=1.9.0 --extra-index-url https://download.pytorch.org/whl/cpu torchvision>=0.10.0 --extra-index-url https://download.pytorch.org/whl/cpu pytest==7.1.3 tqdm==4.64.1 scipy>=1.7.3 matplotlib>=3.4.3 mock==4.0.3
+
+RUN pip install imageai --upgrade
 
 # Copiar todo o restante dos arquivos do projeto para o diretório de trabalho no container
 COPY . .
@@ -22,4 +25,4 @@ COPY . .
 EXPOSE 8080
 
 # Comando padrão para iniciar a aplicação Python (app.py)
-CMD ["python", "app.py"]
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080"]
--- app.py
+++ app.py
@@ -1,82 +1,37 @@
-# Importa as classes Flask, jsonify e request do módulo Flask
-from flask import Flask, jsonify, request
+from fastapi import FastAPI
+from imageai.Detection import ObjectDetection
+import os
+from fastapi.responses import JSONResponse
 
-# Importa o pacote NumPy para manipulação de arrays
-import numpy as np
+app = FastAPI()
 
-# Importa o OpenCV para manipulação de imagens
-import cv2
+execution_path = os.getcwd()
 
-# Importa o módulo base64 para decodificação de dados base64
-import base64
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(execution_path, "yolov3.pt"))
+detector.loadModel()
 
-# Importa o modelo pré-treinado ResNet50 da biblioteca Keras
-from keras.applications.resnet50 import ResNet50
+@app.get("/detect")
+def detect_objects():
+    input_image_path = os.path.join(execution_path, "sample.jpg")
+    output_image_path = os.path.join(execution_path, "imagenew.jpg")
 
-# Importa o módulo de pré-processamento de imagens da Keras
-from keras.preprocessing import image
-
-# Importa as funções de pré-processamento e decodificação de predições do ResNet50
-from keras.applications.resnet50 import preprocess_input, decode_predictions
-
-
-# Cria uma instância da aplicação Flask
-app = Flask(__name__)
-
-# Define uma rota na raiz ("/") que aceita somente requisições POST
-@app.route("/", methods=['POST'])
-def hello():
-    # Função que será executada quando uma requisição POST for recebida na rota "/"
+    # Realiza a detecção de objetos
+    detections = detector.detectObjectsFromImage(
+        input_image=input_image_path, 
+        output_image_path=output_image_path, 
+        minimum_percentage_probability=30
+    )
     
-    # Exibe uma mensagem no console indicando o início da execução do modelo
-    print('Começando execução do modelo')
+    # Retorna os resultados da detecção
+    results = [
+        {
+            "name": eachObject["name"],
+            "percentage_probability": eachObject["percentage_probability"],
+            "box_points": eachObject["box_points"]
+        }
+        for eachObject in detections
+    ]
     
-    # Carrega o modelo pré-treinado ResNet50 com os pesos treinados no dataset ImageNet
-    model = ResNet50(weights='imagenet')
-
-    # Obtém a string base64 da imagem enviada na requisição (ou None se não houver)
-    base64_string = request.json.get('image', None)
-
-    # Se a string base64 for None, retorna uma resposta JSON com uma mensagem de erro
-    if base64_string is None:
-        return jsonify({'error': 'No image data found'})
-
-    # Decodifica a string base64 para dados de imagem binários
-    img_data = base64.b64decode(base64_string)
-    
-    # Converte os dados binários para um array NumPy de tipo uint8
-    np_arr = np.frombuffer(img_data, np.uint8)
-
-    # Decodifica o array NumPy em uma imagem usando OpenCV, assumindo que a imagem é colorida
-    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-    # Redimensiona a imagem para 224x224 pixels, que é o tamanho de entrada esperado pelo modelo ResNet50
-    img = cv2.resize(img, (224, 224))
-
-    # Converte a imagem em um array NumPy de formato de array de imagem Keras
-    x = image.img_to_array(img)
-
-    # Adiciona uma dimensão extra ao array para representar o batch size, necessário para a predição
-    x = np.expand_dims(x, axis=0)
-
-    # Pré-processa o array de imagem de entrada de acordo com o que o modelo espera
-    x = preprocess_input(x)
-
-    # Faz a predição utilizando o modelo ResNet50
-    predictions = model.predict(x)
-
-    # Decodifica e imprime as top 3 predições, exibindo a classe e a confiança
-    print(decode_predictions(predictions, top=3))
-
-    # Decodifica as predições e pega apenas as top 3
-    decoded_predictions = decode_predictions(predictions, top=3)[0]
-
-    # Cria uma lista de dicionários contendo o ID da classe, nome da classe e a pontuação de confiança para cada predição
-    result = [{'class_id': pred[0], 'class_name': pred[1], 'score': float(pred[2])} for pred in decoded_predictions]
-
-    # Retorna as predições em formato JSON como resposta
-    return jsonify({'Predicted': result})
-
-# Inicia o servidor Flask, disponível em todas as interfaces de rede na porta 8080, com o modo de debug ativado
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    return JSONResponse(content={"detections": results})
