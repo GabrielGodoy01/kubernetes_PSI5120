7c672f983af61216a64801c6988ab370468fdfac
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-08-11 06:35:47 -0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -13,9 +13,8 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Instalar as dependências Python listadas em requirements.txt
+# Instalar as dependências exigidas pelo ImageAI
 RUN pip install cython pillow>=7.0.0 numpy>=1.18.1 opencv-python>=4.1.2 torch>=1.9.0 --extra-index-url https://download.pytorch.org/whl/cpu torchvision>=0.10.0 --extra-index-url https://download.pytorch.org/whl/cpu pytest==7.1.3 tqdm==4.64.1 scipy>=1.7.3 matplotlib>=3.4.3 mock==4.0.3
-
 RUN pip install imageai --upgrade
 
 # Copiar todo o restante dos arquivos do projeto para o diretório de trabalho no container
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Antes de iniciar a aplicação deve-se fazer o Download do modelo YOLOv3:
+# Antes de iniciar a aplicação, faça o download do modelo YOLOv3:
 # https://github.com/OlafenwaMoses/ImageAI/releases/download/3.0.0-pretrained/yolov3.pt/
 
 from fastapi import FastAPI
@@ -6,38 +6,48 @@ from imageai.Detection import ObjectDetection
 import os
 from fastapi.responses import JSONResponse
 
+# Inicializa a aplicação FastAPI
 app = FastAPI()
 
+# Obtém o caminho atual de execução do script
 execution_path = os.getcwd()
 
+# Cria uma instância do detector de objetos usando ImageAI
 detector = ObjectDetection()
+# Define o tipo de modelo como YOLOv3
 detector.setModelTypeAsYOLOv3()
+# Define o caminho para o modelo YOLOv3 previamente baixado
 detector.setModelPath(os.path.join(execution_path, "yolov3.pt"))
+# Carrega o modelo na memória para uso na detecção de objetos
 detector.loadModel()
 
+# Define o endpoint HTTP GET "/detect" que será acessado para detecção de objetos
 @app.get("/detect")
 def detect_objects():
+    # Define o caminho para a imagem de entrada que será analisada
     input_image_path = os.path.join(execution_path, "sample.jpg")
+    # Define o caminho para a imagem de saída, onde a detecção será marcada
     output_image_path = os.path.join(execution_path, "imagenew.jpg")
 
-    # Realiza a detecção de objetos
+    # Realiza a detecção de objetos na imagem de entrada e salva a imagem de saída com as marcações
     detections = detector.detectObjectsFromImage(
         input_image=input_image_path, 
         output_image_path=output_image_path, 
-        minimum_percentage_probability=30
+        minimum_percentage_probability=30  # Define a probabilidade mínima para que um objeto seja considerado detectado
     )
     
-    # Retorna os resultados da detecção
+    # Processa os resultados da detecção e organiza em um formato JSON
     results = [
         {
-            "name": eachObject["name"],
-            "percentage_probability": eachObject["percentage_probability"],
-            "box_points": eachObject["box_points"]
+            "name": eachObject["name"],  # Nome do objeto detectado
+            "percentage_probability": eachObject["percentage_probability"],  # Probabilidade de acerto da detecção
+            "box_points": eachObject["box_points"]  # Coordenadas da caixa delimitadora do objeto na imagem
         }
         for eachObject in detections
     ]
 
-    # Delete image
+    # Remove a imagem de saída após a detecção (opcional, dependendo da necessidade)
     os.remove(output_image_path)
     
+    # Retorna os resultados da detecção como uma resposta JSON
     return JSONResponse(content={"detections": results})
